a18c0d1b8b02bbaa2d75f9cd06758dbce0e96b47
jdiboll13 <jdiboll@gmail.com>
2017-10-08 09:03:05 -0400

    added some models

---
+++ MovieBorrower/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieBorrower.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ MovieBorrower/Models/MoviesList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieBorrower.Models
+{
+    public class MoviesList
+    {
+        public class Item
+        {
+            public double vote_average { get; set; }
+            public int vote_count { get; set; }
+            public int Id { get; set; }
+            public bool video { get; set; }
+            public string media_type { get; set; }
+            public string title { get; set; }
+            public double popularity { get; set; }
+            public string poster_path { get; set; }
+            public string original_language { get; set; }
+            public string original_title { get; set; }
+            public List<int> genre_ids { get; set; }
+            public string backdrop_path { get; set; }
+            public bool adult { get; set; }
+            public string overview { get; set; }
+            public string release_date { get; set; }
+            public string original_name { get; set; }
+            public string name { get; set; }
+            public string first_air_date { get; set; }
+            public List<string> origin_country { get; set; }
+        }
+
+        public class Movies
+        {
+            public List<Item> items { get; set; }
+        }
+    }
+
+}
